74aa68fe8dae73a9bcf8af2f14c1d5ecdc47f005
Bohdan Tkachenko <tech.bohdan@gmail.com>
2022-04-29 13:20:35 -0500

    Updated webpages

--- Dynamic_website/main.js
+++ Dynamic_website/main.js
@@ -110,45 +110,45 @@ $(window).on("load resize scroll", function() {
   });
 });
 
-const docStyle = document.documentElement.style
-const aElem = document.querySelector('.mainMenuButton')
-const boundingClientRect = aElem.getBoundingClientRect()
-
-aElem.onmousemove = function(e) {
-
-	const x = e.clientX - boundingClientRect.left
-	const y = e.clientY - boundingClientRect.top
-	
-	const xc = boundingClientRect.width/2
-	const yc = boundingClientRect.height/2
-	
-	const dx = x - xc
-	const dy = y - yc
-	
-	docStyle.setProperty('--rx', `${ dy/-1 }deg`)
-	docStyle.setProperty('--ry', `${ dx/10 }deg`)
-	
-}
-
-aElem.onmouseleave = function(e) {
-	
-	docStyle.setProperty('--ty', '0')
-	docStyle.setProperty('--rx', '0')
-	docStyle.setProperty('--ry', '0')
-	
-}
-
-aElem.onmousedown = function(e) {
-	
-	docStyle.setProperty('--tz', '-25px')
-	
-}
-
-document.body.onmouseup = function(e) {
-	
-	docStyle.setProperty('--tz', '-12px')
-	
-}
+//const docStyle = document.documentElement.style
+//const aElem = document.querySelector('.mainMenuButton')
+//const boundingClientRect = aElem.getBoundingClientRect()
+//
+//aElem.onmousemove = function(e) {
+//
+//	const x = e.clientX - boundingClientRect.left
+//	const y = e.clientY - boundingClientRect.top
+//	
+//	const xc = boundingClientRect.width/2
+//	const yc = boundingClientRect.height/2
+//	
+//	const dx = x - xc
+//	const dy = y - yc
+//	
+//	docStyle.setProperty('--rx', `${ dy/-1 }deg`)
+//	docStyle.setProperty('--ry', `${ dx/10 }deg`)
+//	
+//}
+//
+//aElem.onmouseleave = function(e) {
+//	
+//	docStyle.setProperty('--ty', '0')
+//	docStyle.setProperty('--rx', '0')
+//	docStyle.setProperty('--ry', '0')
+//	
+//}
+//
+//aElem.onmousedown = function(e) {
+//	
+//	docStyle.setProperty('--tz', '-25px')
+//	
+//}
+//
+//document.body.onmouseup = function(e) {
+//	
+//	docStyle.setProperty('--tz', '-12px')
+//	
+//}
 
 //// When the user scrolls the page, execute myFunction 
 //window.onscroll = function() {myFunction()};
